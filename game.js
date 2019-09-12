b108396134135869d9380b46355e1253a7905ac2
Stanislav Ch. Nikolov <stanislav.ltb@gmail.com>
2019-09-12 15:55:18 +0300

    Fixed mixed tabs and spaces for indentation and other small styling issues

--- game.js
+++ game.js
@@ -2,23 +2,23 @@
 var myX = 0, myY = 0,updates=0;
 
 function update() {
-    // Napisanoto tuk se izpulnqva otnovo i otnovo mnogo puti v sekunda
-    myX = myX+(mouseX-myX)/10;
-    myY = myY+(mouseY-myY)/10;
+	// Napisanoto tuk se izpulnqva otnovo i otnovo mnogo puti v sekunda
+	myX = myX+(mouseX-myX)/10;
+	myY = myY+(mouseY-myY)/10;
 }
 
 function draw() {
-    // tuk naprogramirai kakvo da se risuva
-    drawImage(backCake, 0, 0, 800, 600);
-    drawImage(femaleAction, myX, myY, 80, 110);
+	// tuk naprogramirai kakvo da se risuva
+	drawImage(backCake, 0, 0, 800, 600);
+	drawImage(femaleAction, myX, myY, 80, 110);
 }
 
 function keyup(key) {
-    // Show the pressed keycode in the console
-    console.log("Pressed", key);
+	// Show the pressed keycode in the console
+	console.log("Pressed", key);
 }
 
 function mouseup() {
-    // Show coordinates of mouse on click
-    console.log("Mouse clicked at", mouseX, mouseY);
+	// Show coordinates of mouse on click
+	console.log("Mouse clicked at", mouseX, mouseY);
 }
